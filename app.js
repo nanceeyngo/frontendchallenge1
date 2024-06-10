1bd6b296767a452c974daacb0640f7f034afa915
Ngozi Philip <nanceeyngo@gmail.com>
2024-06-10 14:04:15 +0100

    updated

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     let menuToggle = document.getElementById ('menu-toggle');
-    let menuLinks = document.getElementById ('menu-links');;
-
+    let menuLinks = document.getElementById ('menu-links');
+    let closeBtn = document.getElementById ('close-btn');
+    
+   
     menuToggle.addEventListener ('click', function() {
-        menuLinks.classList.toggle('active');
+        // menuLinks.classList.toggle('active');
+        menuLinks.classList.add('active');
       
 });
+    closeBtn.addEventListener ('click', function() {
+        menuLinks.classList.remove('active');
+        
+    });
+
     });
     
